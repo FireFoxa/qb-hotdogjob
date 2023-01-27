07e416cebce43e26e86b35aefb564f4a1fb2797f
Dobromir Yonkov <65962127+FireFoxa@users.noreply.github.com>
2023-01-27 16:14:49 +0200

    Update main.lua

--- qb-hotdogjob/server/main.lua
+++ qb-hotdogjob/server/main.lua
@@ -57,51 +57,6 @@ RegisterNetEvent('qb-hotdogjob:server:Sell', function(Amount, Price)
     Player.Functions.AddMoney('cash', tonumber(Amount * Price))
 end)
 
---[[RegisterNetEvent('qb-hotdogjob:server:UpdateReputation', function(quality)
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local JobReputation = Player.PlayerData.metadata["jobrep"]
-
-    if quality == "exotic" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 3 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
-            TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
-            return
-        end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 3
-        else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 3
-        end
-    elseif quality == "rare" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 2 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
-            TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
-            return
-        end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 2
-        else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 2
-        end
-    elseif quality == "common" then
-        if JobReputation["hotdog"] ~= nil and JobReputation["hotdog"] + 1 > Config.MaxReputation then
-            JobReputation["hotdog"] = Config.MaxReputation
-            Player.Functions.SetMetaData("jobrep", JobReputation)
-            TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
-            return
-        end
-        if JobReputation["hotdog"] == nil then
-            JobReputation["hotdog"] = 1
-        else
-            JobReputation["hotdog"] = JobReputation["hotdog"] + 1
-        end
-    end
-    Player.Functions.SetMetaData("jobrep", JobReputation)
-    TriggerClientEvent('qb-hotdogjob:client:UpdateReputation', src, JobReputation)
-end)--]]
 
 -- Commands
 
